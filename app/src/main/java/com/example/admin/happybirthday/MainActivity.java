f8f356ef6ac169b1a58f0c8678e1b0ca29f81853
Admin <fullcmaiti@gmail.com>
2016-09-16 11:58:46 +0200

    Complete project commit

---
+++ app/src/main/java/com/example/admin/happybirthday/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.admin.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+
+
+
+
+}
